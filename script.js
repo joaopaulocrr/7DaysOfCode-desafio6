0f63614ffa2c3becdd946b249a8953289f311b7e
João Paulo <paulocorrentinense@gmail.com>
2023-08-21 07:04:34 -0300

    cv

--- script.js
+++ script.js
@@ -2,65 +2,101 @@ const paragrafoAdicionarAlimento = document.getElementById('paragrafoAdicionarAl
 const body = document.getElementById('corpo')
 const categorias = {}
 function adicionarItem(categoria, item) {
-if (!categorias[categoria]) {
-categorias[categoria] = []; // Inicializa a categoria se ainda não existir
-}
+    if (!categorias[categoria]) {
+        categorias[categoria] = []; // Inicializa a categoria se ainda não existir
+    }
 
-categorias[categoria].push(item); // Adiciona o item à categoria
+    categorias[categoria].push(item); // Adiciona o item à categoria
+}
+function removerItem(categoria, item) {
+    if (!categorias[categoria]) {
+        categorias[categoria] = []; // Inicializa a categoria se ainda não existir
+    }
+
+    categorias[categoria].splice(item); // Adiciona o item à categoria
 }
 
 const yes = document.getElementById('sim')
 const no = document.getElementById('nao')
 const buttonAdd = document.createElement('button')
+const buttonRemove = document.createElement('button')
 let novoItem = ''
 const paragrafoEinput = document.getElementById('paragEinput')
 const selectCategorias = document.getElementById('selectCategorias')
 
 yes.addEventListener('change', () => {
-paragrafoAdicionarAlimento.innerText = 'Qual item você deseja adicionar?'
-const inputItem = document.createElement('input')
-paragrafoEinput.appendChild(inputItem)
-inputItem.style.marginBottom = '12px'
-inputItem.setAttribute('id', 'inputItem')
-inputItem.focus()
+    paragrafoAdicionarAlimento.innerText = 'Qual item você deseja adicionar/remover?'
+    const inputItem = document.createElement('input')
+    paragrafoEinput.appendChild(inputItem)
+    inputItem.style.marginBottom = '12px'
+    inputItem.setAttribute('id', 'inputItem')
+    inputItem.focus()
 
-selectCategorias.style.display ='block'
+    selectCategorias.style.display = 'block'
 
-body.appendChild(buttonAdd)
-buttonAdd.style.display = 'block'
-buttonAdd.textContent = 'Ok'
-buttonAdd.setAttribute('id', 'addButton')
+    body.appendChild(buttonAdd)
+    buttonAdd.textContent = 'Adicionar'
+    buttonAdd.style.backgroundColor = 'lightgreen'
+    buttonAdd.style.cursor = 'pointer'
+    buttonAdd.setAttribute('id', 'addButton')
 
-buttonAdd.addEventListener('click', () => {
-    if(selectCategorias.value == '') {
-        alert("Escolha uma categoria para adicionar o item:")
-    }else{
+    body.appendChild(buttonRemove)
+    buttonRemove.style.backgroundColor = 'red'
+    buttonRemove.style.cursor = 'pointer'
+    buttonRemove.textContent = 'Remover'
+    buttonRemove.setAttribute('id', 'addButton')
 
-        novoItem = inputItem.value
-      
-        
-        adicionarItem(selectCategorias.value, novoItem);
-        console.log(categorias);
+    buttonAdd.addEventListener('click', () => {
+        if (selectCategorias.value == '') {
+            alert("Escolha uma categoria para adicionar/remover o item:")
+        } else {
+            if (inputItem.value == '') {
+                alert('Nenhum item informado!!!')
+            } else {
 
-        inputItem.value = ''
-        inputItem.focus()
+                novoItem = inputItem.value
+                adicionarItem(selectCategorias.value, novoItem);
+                inputItem.value = ''
+            }
+            inputItem.focus()
 
-        for (const prop in categorias) {
-            console.log(`${prop}: ${categorias[prop]}`);
-         }
-    }
-})
+            for (const prop in categorias) {
+                console.log(`${prop}: ${categorias[prop]}`);
+            }
+        }
+    })
+
+    buttonRemove.addEventListener('click', () => {
+        if (selectCategorias.value == '') {
+            alert("Escolha uma categoria para adicionar/remover o item:")
+        } else {
+            if (inputItem.value == '') {
+                alert('Nenhum item informado!!!')
+            } else {
+
+                novoItem = inputItem.value
+                removerItem(selectCategorias.value, novoItem);
+                inputItem.value = ''
+            }
+            inputItem.focus()
+
+            for (const prop in categorias) {
+                console.log(`${prop}: ${categorias[prop]}`);
+            }
+        }
+    })
 })
 //------------//----------//----------------//-----------------------------//----------------------------//-------------
 
 no.addEventListener('change', () => {
 
-paragrafoAdicionarAlimento.innerText = 'Ok! Aqui está a sua lista de compras atualizada:'
+    paragrafoAdicionarAlimento.innerText = 'Ok! Aqui está a sua lista de compras atualizada:'
 
-paragrafoEinput.removeChild(inputItem)
-body.removeChild(buttonAdd)
-selectCategorias.style.display ='none'
-for (const prop in categorias) {
-    paragrafoAdicionarAlimento.innerHTML +=`<p>${prop}: ${categorias[prop]}</p>`
- }
+    paragrafoEinput.removeChild(inputItem)
+    body.removeChild(buttonAdd)
+    body.removeChild(buttonRemove)
+    selectCategorias.style.display = 'none'
+    for (const prop in categorias) {
+        paragrafoAdicionarAlimento.innerHTML += `<p>${prop}: ${categorias[prop]}</p>`
+    }
 })
